38e2ea2fd5738e1483b4766d4619854940b15005
saraalrajabii <saraalrajabii12@gmail.com>
2021-04-21 11:24:47 +0300

    finish saving user and exresise

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,12 +1,118 @@
 import React , {Component} from 'react';
-import { Link} from "react-router-dom";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export default class CreateExercise extends Component{
+ constructor(props){
+     super(props);
 
+     this.state={
+        username:"",
+        description:"",
+        duration:0,
+        date:new Date(),
+        users:[]
+
+     }
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+ }
+ 
+ componentDidMount() {
+
+ }
+
+ onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    })
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    }
+
+    console.log(exercise);
+    
+    window.location = '/';
+  }
     render(){
         return(
-           <div>CreateExercise</div>
+            <div>
+            <h3>Create New Exercise Log</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>Username: </label>
+                <input
+                type="text"
+                    required
+                    className="form-control"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                />
+              </div>
+              <div className="form-group"> 
+                <label>Description: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.description}
+                    onChange={this.onChangeDescription}
+                    />
+              </div>
+              <div className="form-group">
+                <label>Duration (in minutes): </label>
+                <input 
+                    type="text" 
+                    className="form-control"
+                    value={this.state.duration}
+                    onChange={this.onChangeDuration}
+                    />
+              </div>
+              <div className="form-group">
+                <label>Date: </label>
+                <div>
+                  <DatePicker
+                  className="datepicer"
+                    selected={this.state.date}
+                    onChange={this.onChangeDate}
+                  />
+                </div>
+              </div>
+      
+              <div className="form-group">
+                <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+              </div>
+            </form>
+          </div>
         )
     }
 }
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,12 +1,62 @@
 import React , {Component} from 'react';
-import { Link} from "react-router-dom";
 
 
 export default class CreateUser extends Component{
 
-    render(){
-        return(
-           <div>CreateUser</div>
-        )
+    constructor(props){
+        super(props);
+   
+        this.state={
+           user:""
+        }
+       this.onChangeUser = this.onChangeUser.bind(this);
+       this.onSubmit = this.onSubmit.bind(this);
+
+    }
+    
+    componentDidMount() {
+    }
+   
+   
+   
+   
+    onChangeUser(e) {
+       this.setState({
+        user: e.target.value
+       })
+       console.log(this.state.user)
+     }
+   
+     onSubmit(e) {
+        console.log('users');
+       e.preventDefault();
+       const users = {
+        user: this.state.user,
+       }
+       console.log(users);
+       window.location = '/';
+     }
+       render(){
+           return(
+               <div>
+                    <h3>Create New User Log</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>user: </label>
+                <input
+                type="text"
+                    required
+                    className="form-control"
+                    value={this.state.user}
+                    onChange={this.onChangeUser}
+                
+                />
+              </div>
+              <div className="form-group"  >
+                <input type="submit" value="Create user Log" className="btn btn-primary" />
+              </div>
+              </form>
+               </div>
+           )
     }
 }
